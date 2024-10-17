5518ec57ea5f540af4863018c4aab3f4dc8cb81b
Nathan-Simonnet <nathan.simonnet@gmail.com>
2024-10-17 18:09:30 +0200

    Perpective and preserve 3D added

--- app.js
+++ app.js
@@ -1,38 +1,26 @@
-let spamMode = false;
-let spamModeButton = document.getElementById('spam-mode');
+let transitionning = false;
+let previousCard = null;
+let counter = 0;
+let fastMode = null;
 
-spamModeButton.addEventListener('click',() => {
-    spamModeButton.classList.toggle('activated');
-   if( spamModeButton.classList.contains("activated")){
-    spamMode = true;
-   }
+const fastModeButton = document.getElementById('spam-mode');
+fastModeButton.addEventListener('click', () => {
+    fastModeButton.classList.toggle('activated');
+    if (fastModeButton.classList.contains("activated")) {
+        fastMode = 400;
+        fastModeButton.textContent = "Slow mode"
+    } else {
+        fastMode = null;
+        fastModeButton.textContent = "Fast mode"
+    }
 });
 
+const scoreDisplay = document.getElementById('scoreDisplay');
+function counterDisplayer() {
+    scoreDisplay.textContent = counter;
+}
+
 const fruitsArray = ['apple', 'apple', 'banana', 'banana', 'brocoli', 'brocoli', 'cherry', 'cherry', 'pepper', 'pepper', 'straw', 'straw'];
-
-let scoreCounter = 0;
-const scoreDisplay = document.getElementById('scoreDisplay')
-function scoreHandlerAndDisplayer() {
-    // scoreCounter += 0.5;
-    // scoreDisplay.textContent = scoreCounter % 1 === 0 ? scoreCounter : Math.floor(scoreCounter);
-    scoreCounter++;
-    scoreDisplay.textContent = scoreCounter;
-}
-
-let previousCard = null;
-function resetCard(card) {
-    setTimeout(() => {
-        card.classList.remove('flipped');
-        setTimeout(() => {
-            card.firstChild.src = `ressources/question.svg`;
-            card.firstChild.alt = `Image of a question mark`;
-        }, 200);
-        setTimeout(() => {
-            card.addEventListener('click', cardHandler);
-        }, 400)
-    }, 1000)
-}
-
 function arrayRandomisation(array) {
     let fruitsArrayRandomised = [...array];
     for (let i = 0; i < 60; i++) {
@@ -41,145 +29,112 @@ function arrayRandomisation(array) {
         [fruitsArrayRandomised[randomNumberArrayOne], fruitsArrayRandomised[randomNumberArrayTwo]] = [fruitsArrayRandomised[randomNumberArrayTwo], fruitsArrayRandomised[randomNumberArrayOne]];
     }
     return fruitsArrayRandomised
+};
+
+function cardsEventHandler() {
+    const cards = document.querySelectorAll('.card');
+    cards.forEach((card) => {
+
+        function cardsFlipperHandler() {
+            if (!transitionning) {
+
+                if (!card.firstElementChild.classList.contains('active')) {
+
+                    if (previousCard && previousCard.dataset.attr == card.dataset.attr) {
+                        card.firstElementChild.classList.add('active');
+                        previousCard = null;
+                        counter++;
+                        counterDisplayer();
+                    }
+                    else if (previousCard) {
+                        transitionning = true;
+                        card.firstElementChild.classList.add('active');
+                        setTimeout(() => {
+                            card.firstElementChild.classList.remove('active');
+                            previousCard.firstElementChild.classList.remove('active');
+                            transitionning = false;
+                            previousCard = null;
+                        }, fastMode || 1000)
+                        counter++;
+                        counterDisplayer();
+                    } else {
+                        card.firstElementChild.classList.add('active');
+                        previousCard = card;
+                    }
+                }
+            }
+        }
+
+        card.addEventListener('click', cardsFlipperHandler);
+        card.addEventListener('keydown', (e) => {
+            if (e.key === 'Enter') {
+                e.target.click();
+            }
+        });
+    });
 }
-function cardscreaterAndDisplayer(cardsContainer) {
+
+function cardCreationAndDisplaying(reset) {
     const fruitArrayRandomised = arrayRandomisation(fruitsArray);
-    console.log(fruitArrayRandomised)
-
-    for (let i = 0; i < fruitArrayRandomised.length; i++) {
-        const cardContainer = document.createElement('div');
-        cardContainer.classList.add('card-container');
-        cardContainer.setAttribute('tabindex', '0');
-
-        const img = document.createElement('img');
-        img.setAttribute('src', `ressources/question.svg`);
-        img.setAttribute('alt', `image of a question mark`);
-        img.classList.add('card-img');
-        img.dataset.fruit = fruitArrayRandomised[i];
-
-        cardContainer.appendChild(img);
-        cardsContainer.appendChild(cardContainer);
-
-        // cardContainer.addEventListener('click', nono);
-
-        // function nono() {
-        //     const imgFruitFlipped = cardContainer.firstChild
-        //     if (!cardContainer.classList.contains('flipped')) {
-        //         cardContainer.classList.add('flipped');
-        //         cardContainer.removeEventListener('click', nono);
-        //         setTimeout(() => {
-        //             imgFruitFlipped.src = `ressources/${imgFruitFlipped.dataset.fruit}.svg`;
-        //             imgFruitFlipped.alt = `image of a ${imgFruitFlipped.dataset.fruit}`;
-        //         }, 200)
-
-        //         setTimeout(() => {
-        //             cardContainer.classList.remove('flipped');
-        //             setTimeout(() => {
-        //                 imgFruitFlipped.src = `ressources/question.svg`;
-        //                 imgFruitFlipped.alt = `Image of a question mark`;
-        //             }, 200);
-        //             setTimeout(() => {
-        //                 cardContainer.addEventListener('click', nono);
-        //             },400)
-        //         }, 1000)
-        //     }
-        // }
-    }
-}
-function cardsHandler() {
-    const cardsContainer = document.getElementById('cards-container');
-    cardscreaterAndDisplayer(cardsContainer);
-
-    cardsContainer.addEventListener('dragstart', (event) => {
-        event.preventDefault();
-    });
-
-    document.addEventListener('contextmenu', (event) => {
-        event.preventDefault();
-        // return false;
-    });
-
-    // cardsContainer.addEventListener('click', nono);
-
-    // function nono(event) {
-    //     // console.log(event.propertyName)
-    //     if (event.target.classList.contains('card-img')) {
-    //         const imgFruitFlipped = event.target;
-    //         const cardContainer = imgFruitFlipped.parentElement;
-
-    //         if (!cardContainer.classList.contains('flipped')) {
-    //             cardContainer.classList.add('flipped');
-    //             // cardsContainer.removeEventListener('click', nono);
-    //             setTimeout(() => {
-    //                 imgFruitFlipped.src = `ressources/${imgFruitFlipped.dataset.fruit}.svg`;
-    //                 imgFruitFlipped.alt = `image of a ${imgFruitFlipped.dataset.fruit}`;
-    //             }, 200)
-
-    //             // resetCard(cardContainer);
-    //             previousCard = cardContainer;
-    //         }
-    //     }
-    // }
-}
-cardsHandler()
-
-const cards = document.querySelectorAll('.card-container');
-cards.forEach((card) => {
-    card.addEventListener('click', cardHandler);
-});
-
-function cardFlipper(cardContainer, imgFruitFlipped) {
-    cardContainer.classList.add('flipped');
-    setTimeout(() => {
-        imgFruitFlipped.src = `ressources/${imgFruitFlipped.dataset.fruit}.svg`;
-        imgFruitFlipped.alt = `image of a ${imgFruitFlipped.dataset.fruit}`;
-    }, 200)
-}
-
-function cardHandler(event) {
-    if (event.target.classList.contains('card-img')) {
-        const imgFruitFlipped = event.target;
-        const cardContainer = imgFruitFlipped.parentElement;
-
-        if (!cardContainer.classList.contains('flipped') && previousCard) {
-            cardFlipper(cardContainer, imgFruitFlipped)
-
-            if(!spamMode){
-            // Prevent cards spamming
-            cards.forEach((card) => {
-                card.removeEventListener("click", cardHandler);
-                setTimeout(() => {
-                    card.addEventListener("click", cardHandler);
-                }, 800)
-            });
-            }
-
-            if (cardContainer.firstChild.dataset.fruit != previousCard.firstChild.dataset.fruit) {
-                resetCard(cardContainer);
-                resetCard(previousCard);
-            }
-            previousCard = null;
-
-        } else if (!cardContainer.classList.contains('flipped')) {
-            scoreHandlerAndDisplayer()
-            cardFlipper(cardContainer, imgFruitFlipped)
-            cardContainer.removeEventListener('click', cardHandler);
-            previousCard = cardContainer;
+    const gridContainer = document.querySelector('.grid');
+    if (reset) {
+        while (gridContainer.firstChild) {
+            gridContainer.removeChild(gridContainer.firstChild);
         }
     }
+
+    for (let i = 0; i < fruitArrayRandomised.length; i++) {
+        const card = document.createElement('div');
+        card.classList.add('card');
+        card.setAttribute('data-attr', fruitArrayRandomised[i]);
+        card.setAttribute('tabindex', 0);
+
+        const doubleFace = document.createElement('div');
+        doubleFace.classList.add('double-face');
+
+        const frontFace = document.createElement('div');
+        frontFace.classList.add('face', 'front');
+
+        const frontImg = document.createElement('img');
+        frontImg.src = `ressources/${fruitArrayRandomised[i]}.svg`;
+
+        frontFace.appendChild(frontImg);
+
+        const backFace = document.createElement('div');
+        backFace.classList.add('face', 'back');
+
+        const backImg = document.createElement('img');
+        backImg.src = 'ressources/question.svg';
+        backImg.classList.add('back-img');
+
+        backFace.appendChild(backImg);
+        doubleFace.appendChild(frontFace);
+        doubleFace.appendChild(backFace);
+        card.appendChild(doubleFace);
+        gridContainer.appendChild(card)
+
+    }
+    cardsEventHandler();
 }
+cardCreationAndDisplaying()
+
+
+function handleReset() {
+    cardCreationAndDisplaying('reset');
+    counter = 0;
+    counterDisplayer()
+}
+const resetBtn = document.getElementById('reset');
+resetBtn.addEventListener('click', handleReset);
+resetBtn.addEventListener('keydown', (e) => {
+    if (e.key === 'Enter') {
+        handleReset();
+    };
+});
 
 window.addEventListener('keydown', (e) => {
-    if (e.key == " ") {
-      cards.forEach((card) => {
-            card.classList.remove('flipped');
-            setTimeout(() => {
-                card.firstChild.src = `ressources/question.svg`;
-                card.firstChild.alt = `Image of a question mark`;
-            }, 200);
-              card.addEventListener('click', cardHandler);
-      });
-      scoreCounter = 0;
-      scoreDisplay.textContent = 0;
-    }
+    if (e.key === ' ') {
+        e.preventDefault()
+        handleReset();
+    };
 });
